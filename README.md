d4b538748ac60dc5970551845aab3bcc9d16ef98
Markimg22 <marquim.net.221200@hotmail.com>
2024-12-09 13:33:47 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Consultar Parcelas SIGEF
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export function App(): JSX.Element {
+    return (
+        <main>
+            <h1>Consultar Parcelas SIGEF</h1>
+        </main>
+    );
+}
